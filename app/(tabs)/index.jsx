05b3fc9ecdf02de5a734a4272ee1700bf663c386
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-11-15 09:58:07 +0200

    innitial commit

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+
+const Index = () => {
+  return (
+    <View className="flex-1 justify-center items-center bg-[#25292e]">
+      <Text className="text-white text-4xl font-bold">Index</Text>
+    </View>
+  );
+};
+
+export default Index;
